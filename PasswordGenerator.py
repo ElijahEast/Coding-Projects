a2a9dee13c78339b0f111c9dd23ca0bc649d1334
ElijahEast <elijadeeast@gmail.com>
2025-01-20 01:29:04 -0800

    Updated

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -2,12 +2,16 @@ import streamlit as st
 import random
 import string
 
-# Define available characters for the password
-characters = string.ascii_letters + string.digits + string.punctuation
 
 # Streamlit app layout
 st.title("Random Password Generator")
 st.write("How many characters do you want in your password?")
+
+punct = st.checkbox("Do not allow special characters (punctuation)", value = False)
+if (punct):
+    characters = string.ascii_letters + string.digits
+else:
+    characters = string.ascii_letters + string.digits + string.punctuation
 
 # Input for the number of characters in the password
 num = st.number_input("Enter the number of characters", min_value=1, max_value=16, value=12, step=1)
